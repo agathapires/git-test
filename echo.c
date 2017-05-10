de47992e2d99b90e7313f4deacaf2ef8dd82afc4
Ágatha Pires <agathapires@poli.ufrj.br>
2017-05-10 16:47:04 -0300

    Ecoa os argumentos passados na linha de comando

--- echo.c
+++ echo.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(void){
-    printf("hello world!");
-     return 0;
+int main(int argc, char *argv[]){
+    int i;
+    for(i = 0; i < argc; i++)
+        printf("%s", argv[i]);
+
+    printf("\n");
+    return 0;
 }
     
